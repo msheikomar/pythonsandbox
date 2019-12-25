2f2c560f15c84a1dff5b450d49545aca420141bb
msheikomar <msheikomar@paloaltonetworks.com>
2019-12-25 21:50:18 +0530

    Add new code

--- Python/B02_Strings_WorkingWithTextualData.py
+++ Python/B02_Strings_WorkingWithTextualData.py
@@ -4,6 +4,7 @@
 
 message = "Hello World"
 print(len(message))  # To find length of the string
+print(len(message))  # To find length of the string
 print(message[0])  # To access first character of the string
 print(message[10])  # To access any char of string
 # print(message[11])  # **To get index out of range exception
@@ -12,11 +13,17 @@ print(message[:5])  # Alternate way to get first five char of string
 print(message[6:])  # To get char from mid of the string and all the way to the end
 print(message.lower())  # To lower case the string
 print(message.upper())  # To upper case the string
-print(message.count('Hello'))  # To count certain number of char in the string.Use count method.This method takes string as a argument
-print(message.count('l'))  # To get number of times 'L'present in the string
-print(message.find('World'))  # To find index of certain char use find method
-print(message.find('Universe'))  # To find the index of char does not exists will get negative index
-new_message = message.replace('World', 'Universe')  # To replace old string with new string
+print(
+    message.count("Hello")
+)  # To count certain number of char in the string.Use count method.This method takes string as a argument
+print(message.count("l"))  # To get number of times 'L'present in the string
+print(message.find("World"))  # To find index of certain char use find method
+print(
+    message.find("Universe")
+)  # To find the index of char does not exists will get negative index
+new_message = message.replace(
+    "World", "Universe"
+)  # To replace old string with new string
 print(new_message)
 
 message1 = "HELlo WORLD"
